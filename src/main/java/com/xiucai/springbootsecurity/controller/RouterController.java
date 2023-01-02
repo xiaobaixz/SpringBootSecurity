32f833e5fde9fe7d164d06f3bc5a8e06bd64613e
xiucai <184364227@qq.com>
2023-01-02 17:00:51 +0800

    feat: 项目初始化

---
+++ src/main/java/com/xiucai/springbootsecurity/controller/RouterController.java
@@ -0,0 +1,39 @@
+package com.xiucai.springbootsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author xiucai
+ * @date 2023/1/2 11:48
+ * @description 路由的Controller
+ */
+@Controller
+public class RouterController {
+
+    @RequestMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping({"/toLogin"})
+    public String toLogin() {
+        return "views/login";
+    }
+
+    @RequestMapping({"/level1/{id}"})
+    public String level1(@PathVariable("id") int id) {
+        return "views/level1/" + id;
+    }
+
+    @RequestMapping({"/level2/{id}"})
+    public String level2(@PathVariable("id") int id) {
+        return "views/level2/" + id;
+    }
+
+    @RequestMapping({"/level3/{id}"})
+    public String level3(@PathVariable("id") int id) {
+        return "views/level3/" + id;
+    }
+}
